70cbd15d1bd4006da3fec19c69aba7bae1451bb6
omarturmun <93262369+omarturmun@users.noreply.github.com>
2021-11-17 14:06:02 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
-basic.forever(function () {
-	
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    punto.change(LedSpriteProperty.Y, -1)
 })
+input.onButtonPressed(Button.A, function () {
+    punto.move(-1)
+})
+input.onButtonPressed(Button.AB, function () {
+    punto.change(LedSpriteProperty.Y, 1)
+})
+input.onButtonPressed(Button.B, function () {
+    punto.move(1)
+})
+let punto: game.LedSprite = null
+punto = game.createSprite(2, 4)
